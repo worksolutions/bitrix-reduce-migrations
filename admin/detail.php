d756129460ee571a413f09177b9c15e345d6216f
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-24 11:58:49 +0300

    Чистка детальной страницы миграции

--- admin/detail.php
+++ admin/detail.php
@@ -27,13 +27,7 @@ $localization;
 $module = \WS\ReduceMigrations\Module::getInstance();
 
 $serviceLabels = array(
-    '~reference',
-    '~property_list_values',
-    'Reference fix',
-    'reference',
     'group',
-    'dbVersion',
-    'Insert reference'
 );
 
 $label = $_GET['label'];
@@ -45,14 +39,13 @@ switch ($type) {
         $models = AppliedChangesLogModel::find(array('filter' => array('=groupLabel' => $label)));
         $models[0] && $arTitle = array(
             '#date' => $models[0]->date->format('d.m.Y'),
-            '#source' => $models[0]->source,
+            '#source' => $models[0]->hash,
             '#deployer' => $models[0]->getSetupLog()->shortUserInfo()
         );
         $data = array_map(function (AppliedChangesLogModel $model) {
             return array(
                 'description' => $model->description,
                 'updateData' => $model->updateData,
-                'originalData' => $model->originalData
             );
         }, $models);
         break;
@@ -68,32 +61,13 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_aft
 $tabs = array(
     array(
         "DIV" => "edit1",
-        "TAB" => $localization->getDataByPath('tabs.diff'),
-        "ICON" => "iblock",
-        "TITLE" => $localization->getDataByPath('tabs.diff'),
-    ),
-    array(
-        "DIV" => "edit2",
         "TAB" => $localization->getDataByPath('tabs.final'),
         "ICON" => "iblock",
         "TITLE" => $localization->getDataByPath('tabs.final')
     )
 );
-$hasOriginalData = false;
-foreach ($data as $iData) {
-    if ($iData['originalData']) {
-        $hasOriginalData = true;
-        break;
-    }
-}
-if ($hasOriginalData) {
-    $tabs[] = array(
-        "DIV" => "edit3",
-        "TAB" => $localization->getDataByPath('tabs.merge'),
-        "ICON" => "iblock",
-        "TITLE" => $localization->getDataByPath('tabs.merge')
-    );
-}
+
+
 $tabControl = new CAdminTabControl('ws_maigrations_label_detail', $tabs);
 $fSection = function ($label) use ($serviceLabels, $localization) {
     if (in_array($label, $serviceLabels, true)) {
@@ -122,44 +96,6 @@ $fRow = function ($label, $value) use ($serviceLabels, $localization) {
 };
 ShowNote($localization->getDataByPath('description'));
 $tabControl->Begin();
-$tabControl->BeginNextTab();
-foreach ($data as $iData) {
-    if (!$iData['originalData']) {
-        $diff = $iData['updateData'];
-    }
-    if (!$iData['updateData']) {
-        $diff = $iData['originalData'];
-    }
-    if ($iData['updateData'] && $iData['originalData']) {
-        $diff = $fDiff($iData['updateData'], $iData['originalData']);
-    }
-    if (is_scalar($iData['updateData'])) {
-        $diff = array('ID' => $iData['updateData']);
-    }
-    if (!array_filter($diff)) {
-        continue;
-    }
-    $fSection($iData['description']);
-    foreach ($diff as $field => $value) {
-        if (!$value) {
-            continue;
-        }
-        if (is_array($value)) {
-            if (!array_filter($value)) {
-                continue;
-            }
-            $fRow($field);
-            foreach ($value as $subValueField => $subValue) {
-                if (!$subValue) {
-                    continue;
-                }
-                $fRow($subValueField, $subValue);
-            }
-            continue;
-        }
-        $fRow($field, $value);
-    }
-}
 
 $tabControl->BeginNextTab();
 foreach ($data as $iData) {
@@ -187,32 +123,7 @@ foreach ($data as $iData) {
         $fRow($field, $value);
     }
 }
-$tabControl->BeginNextTab();
-foreach ($data as $iData) {
-    if (!$iData['originalData']) {
-        continue;
-    }
-    if (is_scalar($iData['originalData'])) {
-        $iData['originalData'] = array('ID' => $iData['originalData']);
-    }
-    $fSection($iData['description']);
-    foreach ($iData['originalData'] as $field => $value) {
-        if (!$value) {
-            continue;
-        }
-        if (is_array($value)) {
-            $fRow($field, '');
-            foreach ($value as $subValueField => $subValue) {
-                if (!$subValue) {
-                    continue;
-                }
-                $fRow($subValueField, $subValue);
-            }
-            continue;
-        }
-        $fRow($field, $value);
-    }
-}
+
 $tabControl->EndTab();
 $tabControl->Buttons();
 $tabControl->End();
