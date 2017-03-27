badeb4d3ef90b766c89c5f8903480aaa7bfb6bef
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-27 17:15:28 +0300

    Обновление конкретной миграции. Вывод хеша миграции в списке

--- lib/console/command/listcommand.php
+++ lib/console/command/listcommand.php
@@ -22,7 +22,7 @@ class ListCommand extends BaseCommand{
             /** @var ScriptScenario $notAppliedScenario */
             foreach ($list as $notAppliedScenario) {
                 $time += (double)$notAppliedScenario::approximatelyTime();
-                $this->registerFix($notAppliedScenario::name());
+                $this->registerFix($priority, $notAppliedScenario::name(), $notAppliedScenario::hash());
                 $has = true;
             }
         }
@@ -30,14 +30,31 @@ class ListCommand extends BaseCommand{
         $has && $this->printRegisteredFixes($time);
     }
 
-    private function registerFix($name) {
-        $this->registeredFixes[] = $name;
+    private function registerFix($priority, $name, $hash) {
+        $this->registeredFixes[$priority][] = array(
+            'name' => $name,
+            'hash' => $hash,
+        );
     }
 
     private function printRegisteredFixes($time) {
+        //todo: make it prettier
         $this->console->printLine('List of migrations:');
-        foreach ($this->registeredFixes as $name) {
-            $this->console->printLine("   " . $name);
+        $maxLen = 0;
+        foreach ($this->registeredFixes as $fixList) {
+            foreach ($fixList as $fix) {
+                if ($maxLen < strlen($fix['name'])) {
+                    $maxLen = strlen($fix['name']);
+                }
+            }
+        }
+        $mask = "     %-#maxLen#.#maxLen#s %s";
+        $mask = str_replace('#maxLen#', $maxLen + 1, $mask);
+        foreach ($this->registeredFixes as $priority => $fixList) {
+            $this->console->printLine(" $priority");
+            foreach ($fixList as $fix) {
+                $this->console->printLine(sprintf($mask, $fix['name'], substr($fix['hash'], 0, 8)));
+            }
         }
         if ($time) {
             $this->console->printLine("Approximately applying time: $time min");
