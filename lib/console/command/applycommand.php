7d914b649e719df48b86b035e58d90db3e2eb2a7
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-28 10:55:02 +0300

    Применение миграции по хэшу

--- lib/console/command/applycommand.php
+++ lib/console/command/applycommand.php
@@ -3,46 +3,74 @@
 namespace WS\ReduceMigrations\Console\Command;
 
 use WS\ReduceMigrations\Console\Console;
-use WS\ReduceMigrations\Module;
+use WS\ReduceMigrations\Console\ConsoleException;
 
 class ApplyCommand extends BaseCommand{
     /** @var  bool */
     private $force;
     /** @var  bool */
     private $skipOptional;
+    /** @var  string */
+    private $migrationHash;
 
     protected function initParams($params) {
-        $this->force = in_array('-f', $params);
-        $this->skipOptional = in_array('--skip-optional', $params);
+        $this->force = isset($params['-f']) ? $params['-f'] : false;
+        $this->skipOptional = isset($params['--skip-optional']) ? $params['--skip-optional'] : false;
+        $this->migrationHash = isset($params[0]) ? $params[0] : null;
+
     }
 
     public function execute($callback = false) {
-        if (!$this->force) {
-            $this->console
-                ->printLine("Are you sure? (yes|no):");
-            $answer = $this->console->readLine();
-            if ($answer != 'yes') {
-                exit();
-            }
-        }
+        $this->confirmAction();
 
-        if (!$this->module->getPlatformVersion()->isValid()) {
-            $this->console
-                ->printLine("Diagnostic is not valid");
-            exit();
-        }
+        $this->validatePlatform();
+
         $this->console
-            ->printLine("Applying new fixes started....", Console::OUTPUT_PROGRESS);
+            ->printLine("Applying new migrations started....", Console::OUTPUT_PROGRESS);
 
         $time = microtime(true);
 
-        $count = (int)$this->module
-            ->applyNewMigrations($this->skipOptional, $callback);
+        try {
+            if ($this->migrationHash) {
+                $count = 1;
+                $this->module
+                    ->applyMigrationByHash($this->migrationHash, $callback);
+            } else {
+                $count = (int)$this->module
+                    ->applyNewMigrations($this->skipOptional, $callback);
+            }
+        } catch (\Exception $e) {
+            throw new ConsoleException($e->getMessage());
+        }
+
 
         $interval = round(microtime(true) - $time, 2);
 
         $this->console
             ->printLine("Apply action finished! $count items, time $interval sec", Console::OUTPUT_PROGRESS);
+    }
+
+    private function confirmAction() {
+        if ($this->force) {
+            return true;
+        }
+
+        $this->console
+            ->printLine("Are you sure? (yes|no):");
+
+        $answer = $this->console->readLine();
+
+        if ($answer != 'yes') {
+            exit();
+        }
+    }
+
+    private function validatePlatform() {
+        if (!$this->module->getPlatformVersion()->isValid()) {
+            $this->console
+                ->printLine("Diagnostic is not valid");
+            exit();
+        }
     }
 
 }
