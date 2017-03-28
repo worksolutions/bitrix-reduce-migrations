1b66563e2afb1222c301a39fb7b60ad251fde5b3
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-28 11:06:35 +0300

    Перенос вспомогательных функций модуля в include файл

--- lib/module.php
+++ lib/module.php
@@ -434,18 +434,3 @@ class Module {
     }
 
 }
-
-function jsonToArray($json) {
-    global $APPLICATION;
-    $value = json_decode($json, true);
-    $value = $APPLICATION->ConvertCharsetArray($value, "UTF-8", LANG_CHARSET);
-
-    return $value;
-}
-
-function arrayToJson($data) {
-    global $APPLICATION;
-    $data = $APPLICATION->ConvertCharsetArray($data, LANG_CHARSET, "UTF-8");
-
-    return json_encode($data);
-}
