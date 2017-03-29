3917a7ac2f201e5be36a9b31f6cd4e6305005ddc
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-29 12:42:34 +0300

    Консольный rollback

--- lib/console/command/rollbackcommand.php
+++ lib/console/command/rollbackcommand.php
@@ -8,39 +8,56 @@ use WS\ReduceMigrations\Console\ConsoleException;
 class RollbackCommand extends BaseCommand {
 
     private $migrationHash;
+    private $count;
+    private $toHash;
 
     protected function initParams($params) {
         $this->migrationHash = isset($params[0]) ? $params[0] : null;
+        $this->count = isset($params['--count']) ? (int)$params['--count'] : null;
+        if ($this->count && $this->count < 0) {
+            $this->count = 0;
+        }
+        $this->toHash = isset($params['--to-hash']) ? $params['--to-hash'] : null;
     }
 
     public function execute($callback = false) {
-        $this->console
-            ->printLine("Are you sure? (yes|no):");
-
-        $answer = $this->console
-            ->readLine();
-
-        if ($answer != 'yes') {
-            return;
-        }
-        $this->console
-            ->printLine("Rollback action started...", Console::OUTPUT_PROGRESS);
         $start = microtime(true);
         try {
             if ($this->migrationHash) {
+                $this->confirm("Rollback migration with hash={$this->migrationHash}.");
                 $this->module->rollbackByHash($this->migrationHash);
+            } elseif ($this->count) {
+                $this->confirm("Rollback last {$this->count} migrations.");
+                $this->module->rollbackLastFewMigrations($this->count, $callback);
+            } elseif ($this->toHash) {
+                $this->confirm("Rollback migrations to hash={$this->toHash}.");
+                $this->module->rollbackToHash($this->toHash, $callback);
             } else {
-                $this->module
-                    ->rollbackLastChanges($callback);
+                $this->confirm("Rollback last batch.");
+                $this->module->rollbackLastBatch($callback);
             }
         } catch (\Exception $e) {
             throw new ConsoleException($e->getMessage());
         }
 
-
         $interval = round(microtime(true) - $start, 2);
 
         $this->console
             ->printLine("Rollback action finished! Time $interval sec", Console::OUTPUT_PROGRESS);
+    }
+
+    private function confirm($message) {
+        $this->console
+            ->printLine($message . " Are you sure? (yes|no):");
+
+        $answer = $this->console
+            ->readLine();
+
+        if ($answer != 'yes') {
+            throw new ConsoleException('Operation cancelled');
+        }
+
+        $this->console
+            ->printLine("Rollback action started...", Console::OUTPUT_PROGRESS);
     }
 }
