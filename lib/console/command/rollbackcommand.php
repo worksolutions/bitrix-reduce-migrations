e90d9a90834b7af00d4f6458bc05490351a2ee12
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-03-29 12:56:01 +0300

    Консольный rollback

--- lib/console/command/rollbackcommand.php
+++ lib/console/command/rollbackcommand.php
@@ -6,10 +6,15 @@ use WS\ReduceMigrations\Console\Console;
 use WS\ReduceMigrations\Console\ConsoleException;
 
 class RollbackCommand extends BaseCommand {
+    const TYPE_HASH = 0;
+    const TYPE_COUNT = 1;
+    const TYPE_TO_HASH = 2;
+    const TYPE_LAST_BATCH = 3;
 
     private $migrationHash;
     private $count;
     private $toHash;
+    private $type;
 
     protected function initParams($params) {
         $this->migrationHash = isset($params[0]) ? $params[0] : null;
@@ -18,24 +23,27 @@ class RollbackCommand extends BaseCommand {
             $this->count = 0;
         }
         $this->toHash = isset($params['--to-hash']) ? $params['--to-hash'] : null;
+        $this->type = $this->identifyType();
+    }
+
+    private function identifyType() {
+        if ($this->migrationHash) {
+            return self::TYPE_HASH;
+        }
+        if ($this->count) {
+            return self::TYPE_COUNT;
+        }
+        if ($this->toHash) {
+            return self::TYPE_TO_HASH;
+        }
+
+        return self::TYPE_LAST_BATCH;
     }
 
     public function execute($callback = false) {
         $start = microtime(true);
         try {
-            if ($this->migrationHash) {
-                $this->confirm("Rollback migration with hash={$this->migrationHash}.");
-                $this->module->rollbackByHash($this->migrationHash);
-            } elseif ($this->count) {
-                $this->confirm("Rollback last {$this->count} migrations.");
-                $this->module->rollbackLastFewMigrations($this->count, $callback);
-            } elseif ($this->toHash) {
-                $this->confirm("Rollback migrations to hash={$this->toHash}.");
-                $this->module->rollbackToHash($this->toHash, $callback);
-            } else {
-                $this->confirm("Rollback last batch.");
-                $this->module->rollbackLastBatch($callback);
-            }
+            $this->rollback($callback);
         } catch (\Exception $e) {
             throw new ConsoleException($e->getMessage());
         }
@@ -44,6 +52,28 @@ class RollbackCommand extends BaseCommand {
 
         $this->console
             ->printLine("Rollback action finished! Time $interval sec", Console::OUTPUT_PROGRESS);
+    }
+
+    private function rollback($callback = false) {
+        switch ($this->type) {
+            case self::TYPE_HASH:
+                $this->confirm("Rollback migration with hash={$this->migrationHash}.");
+                $this->module->rollbackByHash($this->migrationHash);
+                break;
+            case self::TYPE_COUNT:
+                $this->confirm("Rollback last {$this->count} migrations.");
+                $this->module->rollbackLastFewMigrations($this->count, $callback);
+                break;
+            case self::TYPE_TO_HASH:
+                $this->confirm("Rollback migrations to hash={$this->toHash}.");
+                $this->module->rollbackToHash($this->toHash, $callback);
+                break;
+            case self::TYPE_LAST_BATCH:
+                $this->confirm("Rollback last batch.");
+                $this->module->rollbackLastBatch($callback);
+                break;
+        }
+
     }
 
     private function confirm($message) {
@@ -60,4 +90,5 @@ class RollbackCommand extends BaseCommand {
         $this->console
             ->printLine("Rollback action started...", Console::OUTPUT_PROGRESS);
     }
+
 }
