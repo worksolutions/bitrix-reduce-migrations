83fc6633a0ede2b846d432788fc13d93a061fd88
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2017-04-04 17:49:03 +0300

    Дизайн cli. Команда в списка миграций

--- lib/console/command/listcommand.php
+++ lib/console/command/listcommand.php
@@ -2,6 +2,7 @@
 
 namespace WS\ReduceMigrations\Console\Command;
 
+use WS\ReduceMigrations\Console\Formatter\Table;
 use WS\ReduceMigrations\Scenario\ScriptScenario;
 
 class ListCommand extends BaseCommand{
@@ -21,7 +22,7 @@ class ListCommand extends BaseCommand{
         foreach ($notAppliedScenarios->groupByPriority() as $priority => $list) {
             /** @var ScriptScenario $notAppliedScenario */
             foreach ($list as $notAppliedScenario) {
-                $this->registerFix($priority, $notAppliedScenario::name(), $notAppliedScenario::hash());
+                $this->registerFix($priority, $notAppliedScenario);
                 $has = true;
             }
         }
@@ -29,32 +30,28 @@ class ListCommand extends BaseCommand{
         $has && $this->printRegisteredFixes($notAppliedScenarios->getApproximateTime());
     }
 
-    private function registerFix($priority, $name, $hash) {
+    /**
+     * @param $priority
+     * @param ScriptScenario $notAppliedScenario
+     */
+    private function registerFix($priority, $notAppliedScenario) {
         $this->registeredFixes[$priority][] = array(
-            'name' => $name,
-            'hash' => $hash,
+            'name' => $notAppliedScenario::name(),
+            'hash' => substr($notAppliedScenario::hash(), 0, 8),
+            'time' => $notAppliedScenario::approximatelyTime() . " min",
         );
     }
 
     private function printRegisteredFixes($time) {
-        //todo: make it prettier
-        $this->console->printLine('List of migrations:');
-        $maxLen = 0;
-        foreach ($this->registeredFixes as $fixList) {
-            foreach ($fixList as $fix) {
-                if ($maxLen < strlen($fix['name'])) {
-                    $maxLen = strlen($fix['name']);
-                }
-            }
-        }
-        $mask = "     %-#maxLen#.#maxLen#s %s";
-        $mask = str_replace('#maxLen#', $maxLen + 1, $mask);
+        $table = new Table('List of migrations:');
+        $table->addRow("\tMigrationName", "Hash", "Approximate time");
         foreach ($this->registeredFixes as $priority => $fixList) {
-            $this->console->printLine(" $priority");
+            $table->addRow(" {$priority}:");
             foreach ($fixList as $fix) {
-                $this->console->printLine(sprintf($mask, $fix['name'], substr($fix['hash'], 0, 8)));
+                $table->addRow("\t" . $fix['name'], $fix['hash'], $fix['time']);
             }
         }
+        $this->console->printLine($table);
         if ($time) {
             $this->console->printLine("Approximately applying time: $time min");
         }
